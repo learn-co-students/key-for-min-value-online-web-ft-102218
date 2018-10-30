3be7495bbefc65df8c6a4ac1a882df39e14aff25
Eric Mayfield <eric@visiblycreative.com>
2018-10-30 12:38:56 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,20 +8,14 @@ def key_for_min_value(name_hash)
     final_key = nil
   else
     name_hash.collect { |key, value|
-      puts hash_value
       if hash_value == 0
         hash_value = value
         final_key = key
-        puts final_key
       elsif value <= hash_value
         hash_value = value
         final_key = key
-        puts "Past first key #{final_key}"
       end
       }
   end
-  puts final_key
   final_key
 end
-
-key_for_min_value({})
