11d8f8de62463ae274c34636a7f43a3d41dadb0f
Eric Mayfield <eric@visiblycreative.com>
2018-10-30 12:39:18 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,19 +3,12 @@
 
 def key_for_min_value(name_hash)
   hash_value = 0
-  final_key = ""
-  if name_hash.empty?
-    final_key = nil
-  else
-    name_hash.collect { |key, value|
-      if hash_value == 0
-        hash_value = value
-        final_key = key
-      elsif value <= hash_value
-        hash_value = value
-        final_key = key
-      end
-      }
-  end
+  final_key = nil
+  name_hash.each { |key, value|
+    if hash_value == 0 || value <= hash_value
+      hash_value = value
+      final_key = key
+    end
+  }
   final_key
 end
