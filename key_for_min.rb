6f16752fb1fb3c4857c41d2313eaac1ad8b750f2
Yuntong Man <yuntongman@Yuntongs-MBP.fios-router.home>
2018-11-25 05:24:12 -0500

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  hash_value = 0
+  curr_value = 0
   final_key = nil
   name_hash.each { |key, value|
-    if hash_value == 0 || value <= hash_value
-      hash_value = value
+    if curr_value == 0 || value <= curr_value
+      curr_value = value
       final_key = key
     end
   }
